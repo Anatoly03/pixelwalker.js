ebb9b541e546f9e414c976cdf47814ba6f6a0f4d
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-19 18:03:57 +0200

    add init

--- src/event.js
+++ src/event.js
@@ -124,6 +124,7 @@ export default class Client extends EventEmitter {
      */
     internal_player_init([id, cuid, username, face, isAdmin, x, y, can_edit, can_god, title, plays, owner, width, height]) {
         this.init()
+        console.log('init')
         this.players.set(id, {
             cuid, username, face, isAdmin, x: x / 16, y: y / 16, god_mode: false, mod_mode: false, has_crown: false
         })
