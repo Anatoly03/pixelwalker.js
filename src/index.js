8289be1dc7718f7de97219750ba9d914e940e659
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-20 17:24:41 +0200

    added world import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
-export {default as default} from './event.js'
+export { default as default } from './event.js'
+export { World } from './world.js'
 export * as Type from './types.js'
 export { MessageType } from './consts.js'
