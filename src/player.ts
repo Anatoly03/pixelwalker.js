b39118aad07a9ecd1f78b17e66fe9ee883dfba62
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-22 13:18:20 +0200

    add player method placeholder

--- src/player.ts
+++ src/player.ts
@@ -1,9 +1,11 @@
 import Client from "./event";
 
 export default class Player {
-    private client: Client
+    private readonly client: Client
 
     constructor(args) {
+        this.client = args.client
+
         // _client = client
         // id = null
         // cuid = null
@@ -15,5 +17,21 @@ export default class Player {
         // god_mode = false
         // mod_mode = false
         // has_crown = false
+    }
+
+    public async pm(content: string) {
+        // TODO /pm
+    }
+
+    public async respond(content: string) {
+        // TODO "USERNAME: message"
+    }
+
+    public async kick(reason: string) {
+        // TODO /kick USERNAME
+    }
+
+    public async edit(value: boolean) {
+        // TODO /giveedit, /takeedit
     }
 }
