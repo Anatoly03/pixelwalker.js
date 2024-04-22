6fc2e101b81e0f804ccc1b7ced31acbaf8d0ae68
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-22 13:15:29 +0200

    add player import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 export { default as default } from './event'
 export { default as World, Block } from './world'
+export { default as Player } from './player'
 export * as Type from './types'
 export { MessageType } from './consts'
