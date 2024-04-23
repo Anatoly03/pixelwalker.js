46413c5eaafd6a1fa2ec01e28a821b0b2729daf1
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-23 19:33:58 +0200

    add isSameAs

--- src/block.ts
+++ src/block.ts
@@ -12,6 +12,14 @@ export default class Block {
         this.id = id
     }
 
+    public isSameAs(other: Block) {
+        if (this.id != other.id) return false
+        if (this.data.length != other.data.length) return false
+        for (let i = 0; i < this.data.length; i++)
+            if (this.data[i] != other.data[i]) return false
+        return true
+    }
+
     public get name(): string {
         return BlockMappingsReverse[this.id]
     }
