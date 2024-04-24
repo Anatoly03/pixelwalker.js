b8638076112e1fb9d58ba74107e8186436eef735
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-24 12:43:14 +0200

    add events to constants

--- src/consts.ts
+++ src/consts.ts
@@ -15,25 +15,31 @@ export const enum HeaderTypes {
 }
 
 export const MessageType = {
-    'init':             0,   // [0x6B, 0],
-    'updateRights':     1,   // [0x6B, 1],
-    'worldMetadata':    2,   // [0x6B, 2],
-    'worldCleared':     3,   // [0x6B, 3],
-    'chatMessage':      4,   // [0x6B, 4],
-    'systemMessage':    5,   // [0x6B, 5],
-    'playerJoined':     6,   // [0x6B, 6],
-    'playerLeft':       7,   // [0x6B, 7],
-    'playerMoved':      8,   // [0x6B, 8],
-    'playerFace':       9,   // [0x6B, 9],
-    'playerGodMode':    10,  // [0x6B, 10],
-    'playerModMode':    11,  // [0x6B, 11],
-    'playerCheckpoint': 12,  // [0x6B, 12],
-    'playerRespawn':    13,  // [0x6B, 13],
-    'placeBlock':       14,  // [0x6B, 14],
-    'crownTouched':     15,  // [0x6B, 15],
-    'keyPressed':       16,  // [0x6B, 16],
-
-    'playerStatsChanged':       19,  // [0x6B, 19],
+    'init':               0,   // [0x6B, 0],
+    'updateRights':       1,   // [0x6B, 1],
+    'worldMetadata':      2,   // [0x6B, 2],
+    'worldCleared':       3,   // [0x6B, 3],
+    'chatMessage':        4,   // [0x6B, 4],
+    'systemMessage':      5,   // [0x6B, 5],
+    'playerJoined':       6,   // [0x6B, 6],
+    'playerLeft':         7,   // [0x6B, 7],
+    'playerMoved':        8,   // [0x6B, 8],
+    'playerFace':         9,   // [0x6B, 9],
+    'playerGodMode':      10,  // [0x6B, 10],
+    'playerModMode':      11,  // [0x6B, 11],
+    'playerCheckpoint':   12,  // [0x6B, 12],
+    'playerRespawn':      13,  // [0x6B, 13],
+    'placeBlock':         14,  // [0x6B, 14],
+    'crownTouched':       15,  // [0x6B, 15],
+    'keyPressed':         16,  // [0x6B, 16],
+    // missing 17
+    // missing 18
+    'playerStatsChanged': 19,  // [0x6B, 19],
+    // missing 20
+    'localSwitchChange':  21,  // [0x6B, 19],
+    'localSwitchReset':   22,  // [0x6B, 19],
+    'globalSwitchChange': 23,  // [0x6B, 19],
+    'globalSwitchReset':  24,  // [0x6B, 19],
 }
 
 export const SpecialBlockData: {[keys: string]: HeaderTypes[]} = {
