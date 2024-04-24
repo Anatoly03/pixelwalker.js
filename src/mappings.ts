8de0d857521ac7fe0772469957f0f5d5dd0fdc00
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-24 20:39:40 +0200

    fix mappings

--- src/mappings.ts
+++ src/mappings.ts
@@ -3,11 +3,9 @@ import { API_ROOM_LINK } from "./consts.js"
 export let BlockMappings: {[keys: string]: number}
 export let BlockMappingsReverse: {[keys: number]: string}
 
-(async () => {
-    const data = await fetch(`https://${API_ROOM_LINK}/mappings`)
-    const text = await data.text()
-    const map = JSON.parse(text)
+const data: Response = await fetch(`https://${API_ROOM_LINK}/mappings`)
+const text = await data.text()
+const map = JSON.parse(text)
 
-    BlockMappings = map
-    BlockMappingsReverse = Object.fromEntries(Object.entries(map).map(a => a.reverse()))
-})()
+BlockMappings = map
+BlockMappingsReverse = Object.fromEntries(Object.entries(map).map(a => a.reverse()))
