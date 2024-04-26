419ec91df4272011fffd029112548eeca376fdef
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-26 16:47:00 +0200

    add comments

--- src/events.ts
+++ src/events.ts
@@ -1,8 +1,12 @@
-import Client from "./client";
-import Player from "./types/player";
+import Client from "./client.js";
+import Player from "./types/player.js";
 
 export default (client: Client) => {
 
+    /**
+     * On player join, create a player object with data
+     * and emit `player:join` with said object.
+     */
     client.raw.on('playerJoined', async (
         [id, cuid, username, face, isAdmin, x, y, god_mode, mod_mode, has_crown]:
             [number, string, string, number, boolean, number, number, boolean, boolean, boolean]
@@ -25,6 +29,10 @@ export default (client: Client) => {
         client.emit('player:join', [player])
     })
 
+    /**
+     * On player leave, send the object of the player
+     * and destroy it.
+     */
     client.raw.on('playerLeft', async ([id]: [number]) => {
         const player = await client.wait_for(() => client.players.get(id))
         client.emit('player:leave', [player])
