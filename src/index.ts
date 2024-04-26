a74a1a9cfd90d7b954bab0823f6233c492fbd330
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-26 09:17:37 +0200

    add world position export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 export { default, default as Client } from './event.js'
 export { default as World } from './world.js'
-export { default as Block } from "./types/block.js"
+export { default as Block, WorldPosition } from "./types/block.js"
 export { default as Player } from './types/player.js'
 export * as Animation from './types/animation.js'
 export * as Type from './types.js'
