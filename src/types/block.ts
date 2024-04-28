3e3703aadbaf2a10e54d910919c6eaa6e76a808a
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-04-28 09:46:59 +0200

    add block.issameas for other types

--- src/types/block.ts
+++ src/types/block.ts
@@ -15,7 +15,9 @@ export default class Block {
         this.id = id
     }
 
-    public isSameAs(other: Block) {
+    public isSameAs(other: Block | string | number) {
+        if (typeof other == 'number') other = new Block(other)
+        if (typeof other == 'string') other = new Block(other)
         if (this.id != other.id) return false
         if (this.data.length != other.data.length) return false
         for (let i = 0; i < this.data.length; i++)
