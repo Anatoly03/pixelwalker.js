53b0f98db648d622028f6b3b4e12921b6bc05074
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-01 11:47:07 +0200

    fix undefined block

--- src/client.ts
+++ src/client.ts
@@ -219,7 +219,7 @@ export default class Client extends EventEmitter<LibraryEvents> {
         if (!(block instanceof Block)) return Promise.resolve(true)
         if (!this.scheduler) {console.log(this); throw new Error('Scheduler is not defined.')}
 
-        if (this.world?.[layer == 1 ? 'foreground' : 'background'][x][y].isSameAs(block)) return Promise.resolve(true)
+        if (this.world?.[layer == 1 ? 'foreground' : 'background'][x][y]?.isSameAs(block)) return Promise.resolve(true)
 
         return this.scheduler.block([x, y, layer], block)
 
