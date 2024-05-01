9be8aaa8db90129f646cea240aef9ceb1937af7f
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-01 09:13:49 +0200

    revert to events.ts

--- src/client.ts
+++ src/client.ts
@@ -12,6 +12,7 @@ import Block, { WorldPosition } from './types/block.js'
 import Player from './types/player.js'
 import { FIFO, RANDOM } from './types/animation.js'
 import { RoomTypes } from './data/room_types.js'
+import init_events from './events.js'
 
 export default class Client extends EventEmitter<LibraryEvents> {
     public readonly raw: EventEmitter<RawGameEvents> = new EventEmitter()
@@ -30,7 +31,7 @@ export default class Client extends EventEmitter<LibraryEvents> {
 
     private move_tick: number = 0
     private intervals: NodeJS.Timeout[] = []
-    private block_queue: Map<`${number}.${number}.${0|1}`, Block> = new Map()
+    public block_queue: Map<`${number}.${number}.${0|1}`, Block> = new Map()
 
     constructor(args: { token?: string, user?: string, pass?: string, flags?: {}, debug?: boolean }) {
         super()
@@ -93,7 +94,7 @@ export default class Client extends EventEmitter<LibraryEvents> {
         this.intervals.push(setInterval(() => this.fill_block_loop(), 25))
         // this.fill_block_loop()
 
-        this.init_events()
+        init_events(this)
     }
 
     /**
@@ -310,214 +311,4 @@ export default class Client extends EventEmitter<LibraryEvents> {
             await this.wait()
         }
     }
-
-    /**
-     * Event Initialiser for Client
-     */
-    private async init_events () {
-
-        /**
-         * On init, set everything up
-         */
-        this.raw.once('init', async ([id, cuid, username, face, isAdmin, x, y, can_edit, can_god, title, plays, owner, global_switch_states, width, height, buffer]) => {
-            await this.send(Magic(0x6B), Bit7(MessageType['init']))
-
-            this.world = new World(width, height)
-            this.world.init(buffer)
-
-            this.self = new Player({
-                client: this,
-                id,
-                cuid,
-                username,
-                face,
-                isAdmin,
-                x: x / 16,
-                y: y / 16,
-            })
-
-            this.players.set(id, this.self)
-            this.emit('start', [this.self])
-        })
-
-        /**
-         * On player join, create a player object with data
-         * and emit `player:join` with said object.
-         */
-        this.raw.on('playerJoined', async ([id, cuid, username, face, isAdmin, x, y, coins, blue_coins, deaths, god_mode, mod_mode, has_crown]) => {
-            const player = new Player({
-                client: this,
-                id,
-                cuid,
-                username,
-                face,
-                isAdmin,
-                x: x / 16,
-                y: y / 16,
-                god_mode,
-                mod_mode,
-                has_crown,
-                coins,
-                blue_coins,
-                deaths
-            })
-
-            this.players.set(id, player)
-            this.emit('player:join', [player])
-        })
-
-        /**
-         * On player leave, send the object of the player
-         * and destroy it.
-         */
-        this.raw.on('playerLeft', async ([id]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-            this.emit('player:leave', [player])
-            this.players.delete(id)
-        })
-
-        /**
-         * When receiving a chat message, if it is a command,
-         * emit command, otherwise emit chat message
-         */
-        this.raw.on('chatMessage', async ([id, message]) => {
-            if (!message) return
-            
-            const player = await this.wait_for(() => this.players.get(id))
-            const prefix = this.cmdPrefix.find(v => message.toLowerCase().startsWith(v))
-
-            if (!prefix) return this.emit('chat', [player, message])
-
-            const slice = message.substring(prefix.length)
-            const arg_regex = /"[^"]+"|'[^']+'|[\w\-]+/gi // TODO add escape char \
-            const args: [Player, ...any] = [player]
-            for (const match of slice.matchAll(arg_regex)) args.push(match[0])
-            if (args.length < 2) return
-
-            const cmd = args[1].toLowerCase()
-
-            this.emit(`cmd:${cmd}`, args)
-        })
-
-        /**
-         * TODO Player movement
-         */
-        this.raw.on('playerMoved', async ([id, x, y, speed_x, speed_y, mod_x, mod_y, horizontal, vertical, space_down, space_just_down, tick_id]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-
-            player.x = x / 16
-            player.y = y / 16
-
-            // TODO if (player.mod_x != undefined && player.mod_x != mod_x) // hit key right or left
-            // TODO if (player.mod_y != undefined && player.mod_y != mod_y) // hit key up or down
-            // TODO hit space
-
-            // TODO
-            // this.emit('player:move', [player])
-
-            player.horizontal = horizontal
-            player.vertical = vertical
-            player.space_down = space_down
-            player.space_just_down = space_just_down
-        })
-
-        /**
-         * When player changes face, update.
-         */
-        this.raw.on('playerFace', async ([id, face]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-            const old_face = player.face
-            player.face = face
-            this.emit('player:face', [player, face, old_face])
-        })
-
-        /**
-         * TODO When player changes god mode, update.
-         */
-        this.raw.on('playerGodMode', async ([id, god_mode]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-            const old_mode = player.god_mode
-            player.god_mode = god_mode
-            this.emit('player:god', [player])
-        })
-
-        /**
-         * TODO When player changes mod mode, update.
-         */
-        this.raw.on('playerModMode', async ([id, mod_mode]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-            const old_mode = player.god_mode
-            player.mod_mode = mod_mode
-            this.emit('player:mod', [player])
-        })
-
-        /**
-         * TODO
-         */
-        this.raw.on('crownTouched', async ([id]) => {
-            const players = await this.wait_for(() => this.players)
-            const player: Player = players.get(id) as Player
-            const old_crown = Array.from(players.values()).find(p => p.has_crown)
-            players.forEach((p) => p.has_crown = p.id == id)
-            this.emit('player:crown', [player, old_crown || null])
-        })
-
-        /**
-         * TODO
-         */
-        this.raw.on('playerStatsChanged', async ([id, gold_coins, blue_coins, death_count]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-
-            const old_coins = player.coins
-            const old_blue_coins = player.blue_coins
-            const old_death_count = player.deaths
-
-            player.coins = gold_coins
-            player.blue_coins = blue_coins
-            player.deaths = death_count
-
-            if (old_coins < gold_coins) this.emit('player:coin', [player, old_coins])
-            if (old_blue_coins < blue_coins) this.emit('player:coin:blue', [player, old_blue_coins])
-            if (old_death_count < death_count) this.emit('player:death', [player, old_death_count])
-        })
-
-        /**
-         * TODO
-         */
-        this.raw.on('placeBlock', async ([id, x, y, layer, bid, ...args]) => {
-            const player = await this.wait_for(() => this.players.get(id))
-            const world = await this.wait_for(() => this.world)
-            const [position, block] = world.place(x, y, layer, bid, args)
-            
-            const key: `${number}.${number}.${0|1}` = `${x}.${y}.${layer}`
-            const entry = this.block_queue.get(key)
-
-            if (this.self && entry && this.self.id == id) {
-                if (this.block_queue.get(key)?.isSameAs(block)) {
-                    this.block_queue.delete(key)
-                }
-            }
-
-            this.emit('player:block', [player, position, block])
-        })
-
-        /**
-         * TODO
-         */
-        this.raw.on('worldCleared', async ([]) => {
-            console.debug('World Reload not yet implemented.')
-            const world = await this.wait_for(() => this.world)
-            world.clear(true)
-            this.emit('world:clear', [])
-        })
-
-        /**
-         * Reload world with new buffer.
-         */
-        this.raw.on('worldReloaded', async ([buffer]) => {
-            const world = await this.wait_for(() => this.world)
-            world.init(buffer)
-        })
-    }
-
 }
