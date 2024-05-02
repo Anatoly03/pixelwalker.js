80532b74b757cee079635c8f16787c7b15228da8
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-02 13:52:35 +0200

    overwrite block types on update

--- src/data/mappings.ts
+++ src/data/mappings.ts
@@ -1,4 +1,5 @@
 import { API_ROOM_LINK } from "./consts.js"
+import fs from 'node:fs'
 
 const data: Response = await fetch(`https://${API_ROOM_LINK}/mappings`)
 const text = await data.text()
@@ -6,3 +7,10 @@ const map: {[keys: string]: number} = JSON.parse(text)
 
 export const BlockMappings: {[keys: string]: number} = map
 export const BlockMappingsReverse: {[keys: number]: string} = Object.fromEntries(Object.entries(map).map(a => a.reverse()))
+
+fs.writeFileSync(import.meta.dirname + '/mappings.d.ts', `
+// This is auto generated in the project.
+
+export declare const BlockMappings: ${JSON.stringify(BlockMappings)}
+export declare const BlockMappingsReverse: ${JSON.stringify(BlockMappingsReverse)}
+`)
