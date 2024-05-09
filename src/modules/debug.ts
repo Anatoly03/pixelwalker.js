c992b08ac55bfe98c884a0ac120b11dbfd125bd1
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-09 09:12:58 +0200

    return client in debug module

--- src/modules/debug.ts
+++ src/modules/debug.ts
@@ -8,5 +8,6 @@ export default (events: (keyof RawGameEvents)[]) => {
     return (client: Client) => {
         for (const event_name of events)
             client.raw.on(event_name, console.debug)
+        return client
     }
 }
