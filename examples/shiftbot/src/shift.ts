b60e5f489325198768b3a80f070ccd3dbae1da45
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-11 21:02:05 +0200

    add ban example

--- examples/shiftbot/src/shift.ts
+++ examples/shiftbot/src/shift.ts
@@ -13,9 +13,11 @@ export default client
 
 import * as Map from './map.js'
 import * as Game from './game.js'
+import * as Ban from './bans.js'
 
 client
     .on('player:join', ([p]) => p.god(true))
     .include(Map)
     .include(Game)
+    .include(Ban)
     .connect(process.env.WORLD_ID)
