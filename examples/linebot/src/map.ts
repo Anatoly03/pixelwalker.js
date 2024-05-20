9c69503f78ed3aa2d96f4bdbbb46901e9ad626c9
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-20 13:21:20 +0200

    fix shiftbot map advancement

--- examples/linebot/src/map.ts
+++ examples/linebot/src/map.ts
@@ -6,6 +6,8 @@ import path from 'node:path'
 
 export const width = 50
 export const height = 37
+
+const empty_map = new Structure(200, 50)
 const map = new Structure(200, 50)
 
 const TOP_LEFT = { x: parseInt(process.env.TLX || '0'), y: parseInt(process.env.TLY || '0') }
@@ -14,32 +16,39 @@ const VERTICAL_BORDER = 10
 
 let FRAME = new Block('beveled_magenta')
 
-const QUEUE: [string, string][] = []
-const MAPS_PATH = process.env.MAPS_PATH || 'maps'
+const QUEUE: [string, Structure][] = []
+const TILES_PATH = process.env.MAPS_PATH || 'maps'
 const JOINT = { x: 0, y: 0 }
-
-let CrowdControlFlip = false // Crowd Control Flip
+const LEFT_JOINT = { x: 0, y: 0 }
+let PLATFORM_SIZE = 40
+let SIZE = 0
 
 function create_empty_arena() {
     map.clear(true)
+    empty_map.clear(true)
 
     const PORTAL_MATRIX = () => [...Array(6).keys()]
         .map((x, i) => [...Array(map.height - 3).keys()].map(i => i + 1))
 
     const SPIKES = [...Array(map.width - 2).keys()]
         .map(i => i + 1)
-        .map(x => map.foreground[x][map.height - 2] = new Block('spikes'))
+        .map(x => {
+            map.foreground[x][map.height - 2] = new Block('spikes')
+            empty_map.foreground[x][map.height - 2] = new Block('spikes')
+        })
 
     const LEFT = PORTAL_MATRIX().forEach((a, x) => a.forEach(y => {
         const portal_left = new Block('portal_invisible')
         portal_left.data = [2, 1000 - (x) * (map.height - 1) - y, 200]
         map.foreground[x + HORIZONTAL_BORDER - 2][y] = portal_left
+        empty_map.foreground[x + HORIZONTAL_BORDER - 2][y] = portal_left
     }))
 
     const RIGHT = PORTAL_MATRIX().forEach((a, x) => a.forEach(y => {
         const portal_right = new Block('portal_invisible')
         portal_right.data = [2, 201, 1000 - (x) * (map.height - 1) - y]
         map.foreground[x + map.width - HORIZONTAL_BORDER - 2][y] = portal_right
+        empty_map.foreground[x + map.width - HORIZONTAL_BORDER - 2][y] = portal_right
     }))
 
     return client.fill(TOP_LEFT.x, TOP_LEFT.y, map)
@@ -53,17 +62,112 @@ function build_platform(LENGTH: number) {
 
     JOINT.x = POSITIONS[POSITIONS.length - 1]
     JOINT.y = y
+    LEFT_JOINT.x = POSITIONS[0]
+    SIZE = LENGTH
+    PLATFORM_SIZE = LENGTH
 
-    return POSITIONS.map(x => client.block(TOP_LEFT.x + x, TOP_LEFT.y + y, 1, FRAME))
+    return Promise.all(POSITIONS.map(x => client.block(TOP_LEFT.x + x, TOP_LEFT.y + y, 1, FRAME)))
+}
+
+function plan_to_queue() {
+    const calculated_y_joint = QUEUE.map(([_, piece]) => piece.meta.right_y - piece.meta.left_y).reduce((p, c) => p += c, JOINT.y)
+
+    for (let i = 0; i < 1000; i++) {
+        const maps = fs.readdirSync(TILES_PATH)
+        const random_piece = maps[Math.floor(Math.random() * maps.length)]
+        const value = fs.readFileSync(`${TILES_PATH}/${random_piece}`).toString()
+        let piece = Structure.fromString(value)
+        if (calculated_y_joint - piece.meta.left_y + piece.height > map.height - VERTICAL_BORDER) continue
+        if (calculated_y_joint - piece.meta.left_y < VERTICAL_BORDER) continue
+        QUEUE.push([random_piece, piece])
+        return
+    }
+
+    console.error(`After 1.000 iterations could not find proper piece: Y_JOINT = ${calculated_y_joint}`)
+}
+
+async function advance_one_piece() {
+    const [_, piece] = QUEUE.shift() || [undefined, undefined]
+    if (!piece) return console.error('`advance_one_piece` was called on empty queue')
+
+    for (let x = 0; x < piece.width; x++) {
+        const line = piece.copy(x, 0, x, piece.height - 1)
+        line.replace_all(new Block('basic_gray'), FRAME)
+
+        if (JOINT.x + x < TOP_LEFT.x + map.width - 1)
+            await client.fill(JOINT.x + x, TOP_LEFT.y + JOINT.y - piece.meta.left_y, line, { write_empty: false })
+
+        if (JOINT.x + x > TOP_LEFT.x + map.width - HORIZONTAL_BORDER * 2) {
+            await client.fill(JOINT.x + x + TOP_LEFT.x - map.width + HORIZONTAL_BORDER * 2, TOP_LEFT.y + JOINT.y - piece.meta.left_y, line, { write_empty: false })
+        }
+
+        if (SIZE++ > PLATFORM_SIZE) {
+            let replace = empty_map.copy(LEFT_JOINT.x, 0, LEFT_JOINT.x, empty_map.height)
+
+            if (LEFT_JOINT.x < TOP_LEFT.x + map.width - 1)
+                client.fill(LEFT_JOINT.x, TOP_LEFT.y, replace)
+
+            if (LEFT_JOINT.x > TOP_LEFT.x + map.width - HORIZONTAL_BORDER * 2) {
+                const offset_x = LEFT_JOINT.x - map.width + HORIZONTAL_BORDER * 2
+                replace = empty_map.copy(offset_x, 0, offset_x, empty_map.height)
+                client.fill(offset_x, TOP_LEFT.y, replace)
+            }
+
+            // else if (left_x < corner.x + offset * 2) {
+            //     const offset_x = left_x - corner.x + map.width - offset
+            //     replace = create_line(offset_x)
+            //     if (offset_x < corner.x + map.width - 1)
+            //         client.fill(offset_x - 1, corner.y + 1, replace)
+            // }
+
+            LEFT_JOINT.x++
+        }
+
+        await client.wait(200)
+        // await client.wait(speed)
+    }
+
+    JOINT.x += piece.width
+
+    if (JOINT.x > TOP_LEFT.x + map.width) {
+        JOINT.x -= map.width - 2 * HORIZONTAL_BORDER
+        // joint.y ++
+        // joint.x = corner.x + offset + platform_size
+    }
+
+    if (LEFT_JOINT.x > TOP_LEFT.x + map.width - 2) {
+        LEFT_JOINT.x = TOP_LEFT.x + HORIZONTAL_BORDER * 2
+    }
+
+    JOINT.y += - piece.meta.left_y + piece.meta.right_y
+
+    // if (speed > 100) speed -= 2
+    // if (speed > 50) speed -= 2
 }
 
 export function module(client: Client) {
-    client.on('cmd:*empty-arena', ([player]) => {
+    client.on('cmd:*empty', ([player]) => {
         return create_empty_arena()
     })
 
-    client.on('cmd:*build-line', ([player]) => {
+    client.on('cmd:*build', ([player]) => {
         return build_platform(30)
+    })
+
+    client.on('cmd:*piece', async ([player, _, z]) => {
+        if (!z) z = '1'
+        let amount: number = parseInt(z)
+        if (Number.isInteger(amount)) {
+            for (let i = amount; i > 0; i--) {
+                plan_to_queue()
+                await advance_one_piece()
+            }
+        }
+    })
+
+    client.on('cmd:*procedure', async ([player]) => {
+        await create_empty_arena()
+        await build_platform(30)
     })
 
     return client
