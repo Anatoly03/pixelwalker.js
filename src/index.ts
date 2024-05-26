c0f84c429160a11306c8d781734026e97cb58655
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-26 16:12:51 +0200

    export block properties

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export * as Animation from './types/animation.js'
 export * as Type from './types.js'
 export { MessageType } from './data/consts.js'
 export { BlockMappings, BlockMappingsReverse } from './data/mappings.js'
-export { SolidBlocks, Decorations } from './data/block_properties.js'
+export { SolidBlocks, Decorations, Property, BlockProperties } from './data/block_properties.js'
 export { RoomTypes } from './data/room_types.js'
 
 export const Modules = {
