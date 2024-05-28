94b1cca84f81bf6224008a40a8294416e4d06691
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-05-28 16:08:15 +0200

    add worm

--- examples/wormbot/src/game.ts
+++ examples/wormbot/src/game.ts
@@ -2,7 +2,7 @@
 import Client, { Player, SolidBlocks, Structure, Util } from '../../../dist/index.js'
 
 import { is_bot_admin } from './worm.js'
-import { TOP_LEFT, build_map } from './map.js'
+import { TOP_LEFT, build_map, width, height } from './map.js'
 import { StoredPlayer } from './storage.js'
 
 export function module(client: Client) {
@@ -10,19 +10,67 @@ export function module(client: Client) {
     
     client.include(gameRound)
 
+    let TICK = 0
     let STRUCTURE: Structure | undefined
     let WORMER = null
     let GAME_IS_STARTING = false
     let GAME_HALT_FLAG = false
     let START_TIME = 0
     let SIGNUP_LOCK: ReturnType<typeof Util.Breakpoint>
-    let WORM = []
+    let WORM: [number, number][] = []
+    let WORM_DIRECTION: 0 | 1 | 2 | 3 = 0
 
     function elect_bomber(player?: Player) {
         if (!player) player = gameRound.players[Math.floor(gameRound.players.length * Math.random())]
         if (!STRUCTURE) return
         const [[x, y]] = STRUCTURE.list('crown')
         return player.teleport(TOP_LEFT.x + x, TOP_LEFT.y + y)
+    }
+
+    function push_worm(x?: number, y?: number) {
+        const [CROWN_COORDINATE] = STRUCTURE?.list('crown') as [number, number, number][] || [10, 10]
+
+        if (WORM.length == 0) {
+            [x, y] = CROWN_COORDINATE
+            y -= 2
+        }
+
+        if (x == undefined || y == undefined)
+            [x, y] = WORM[WORM.length - 1]
+
+        const dx = (WORM_DIRECTION / 2 - .5) * 2,
+              dy = ((WORM_DIRECTION - 1) / 2 - .5) * 2
+
+        if (WORM_DIRECTION % 2 == 0) y += dx
+        if (WORM_DIRECTION % 2 == 1) x += dy
+
+        if (x <= 0) x += width - 2
+        if (y <= 0) y += height - 2
+        if (x >= width) x -= width - 2
+        if (y >= height) y -= height - 2
+
+        WORM.push([x, y])
+
+        if (x < 1 || y < 1 || x > width - 2 || y > height - 2) return Promise.resolve(true) // Outside
+        if (Math.abs(CROWN_COORDINATE[0] - x) <= 1 && Math.abs(CROWN_COORDINATE[1] - y) <= 1) return Promise.resolve(true) // Crown Box
+
+        return client.block(TOP_LEFT.x + x, TOP_LEFT.y + y, 1, 'basic_blue')
+    }
+
+    function pop_worm() {
+        const w = WORM.shift() as [number, number]
+        if (!w) return Promise.resolve(true)
+
+        const [CROWN_COORDINATE] = STRUCTURE?.list('crown') as [number, number, number][] || [10, 10]
+        const [x, y] = w
+
+        if (x < 1 || y < 1 || x > width - 2 || y > height - 2) return Promise.resolve(true) // Outside
+        if (Math.abs(CROWN_COORDINATE[0] - x) <= 1 && Math.abs(CROWN_COORDINATE[1] - y) <= 1) return Promise.resolve(true) // Crown Box
+
+        return Promise.all([
+            client.block(TOP_LEFT.x + x, TOP_LEFT.y + y, 1, 'gravity_dot'),
+            client.block(TOP_LEFT.x + x, TOP_LEFT.y + y, 0, 0),
+        ])
     }
 
     function disqualify(player: Player, code: 'left' | 'god' | 'kill') {
@@ -44,6 +92,7 @@ export function module(client: Client) {
     gameRound.setLoop(async () => {    
         if (GAME_IS_STARTING) {
             WORM = []
+            TICK = 0
 
             STRUCTURE = await build_map()
             const positions = STRUCTURE.list('gravity_dot', 'gravity_slow_dot')
@@ -68,8 +117,16 @@ export function module(client: Client) {
 
             await elect_bomber()
         }
+
+        const promises: Promise<any>[] = [push_worm()]
+        if (WORM.length > 10) promises.push(pop_worm())
+        await Promise.all(promises)
+
+        // if (TICK % 15 == 0) WORM_DIRECTION = Math.floor(Math.random() * 4) as typeof WORM_DIRECTION
+        if (TICK % 15 == 0) WORM_DIRECTION = (Math.abs(WORM_DIRECTION + [-1, 1][+(Math.random() > .5)])) % 4
         
-        await client.wait(200)
+        await client.wait(130)
+        TICK += 1
     })
 
     client.onCommand('start', is_bot_admin, () => {
