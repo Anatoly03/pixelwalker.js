d29339c379b3432139e300bf759de4f69c11e3c5
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-06-01 22:16:30 +0200

    refactor animation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { WorldPosition } from './types/block.js'
 
 export { default, default as Client } from './client.js'
 export { default as World } from './types/world.js'
@@ -5,13 +6,36 @@ export { default as Structure } from './types/structure.js'
 export { default as Block, WorldPosition, BlockIdentifier } from "./types/block.js"
 export { default as Player, PlayerBase, SelfPlayer } from './types/player.js'
 export { PlayerArray, GamePlayerArray } from './types/player-ds.js'
-export * as Animation from './types/animation.js'
 export * as BitType from './types/message-bytes.js'
 export { MessageType } from './data/consts.js'
 export { BlockMappings, BlockMappingsReverse } from './data/mappings.js'
 export { SolidBlocks, Decorations, Property, BlockProperties } from './data/block_properties.js'
 export { RoomTypes } from './data/room_types.js'
 export { default as PlayerStorage } from './modules/player-storage.js'
+
+/**
+ * @namespace
+ * 
+ * The Animation namespace is a collection of predefined common use map filling animations.
+ * An Animation is a generator function, that yields over an array of block data the appropriate placement order.
+ * 
+ * @type {(blocks: [WorldPosition, Block][]) => Generator<[WorldPosition, Block] | undefined, boolean, unknown>}
+ * 
+ * @example Random Placement Animation
+ * 
+ * ```ts
+ * function* RANDOM (to_be_placed: [WorldPosition, Block][]) {
+ *     while (to_be_placed.length > 0) {
+ *         const index = Math.floor(to_be_placed.length * Math.random())
+ *         const block = to_be_placed[index]
+ *         to_be_placed.splice(index, 1)
+ *         yield block
+ *     }
+ *     return true
+ * }
+ * ```
+ */
+export * as Animation from './types/animation.js'
 
 /**
  * @namespace
