a748793139d0cf8f0d333dc860e37e2f42265888
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-08-14 19:32:12 +0200

    add constants and static data retrieved from the server

---
+++ src/data/block-mappings.ts
@@ -0,0 +1,22 @@
+import { APIServerLink } from '../client.js';
+import fs from 'node:fs';
+
+const data: Response = await fetch(`https://${APIServerLink}/mappings`);
+const map: { [keys: string]: number } = await data.json();
+
+export const BlockMappings: { [keys: string]: number } = map;
+export const BlockMappingsReverse: { [keys: number]: string } =
+    Object.fromEntries(Object.entries(map).map((a) => a.reverse()));
+
+if (import.meta.dirname)
+    fs.writeFileSync(
+        import.meta.dirname + '/block-mappings.d.ts',
+        `
+    // This is auto generated in the project.
+
+    export declare const BlockMappings: ${JSON.stringify(BlockMappings)}
+    export declare const BlockMappingsReverse: ${JSON.stringify(
+        BlockMappingsReverse
+    )}
+    `
+    );
---
+++ src/data/message-types.ts
@@ -0,0 +1,19 @@
+import { APIServerLink } from '../client.js';
+import fs from 'node:fs';
+
+const data: Response = await fetch(`https://${APIServerLink}/message_types`);
+const map: string[] = await data.json();
+
+export const MessageTypes: string[] = map;
+export default MessageTypes;
+
+if (import.meta.dirname)
+    fs.writeFileSync(
+        import.meta.dirname + '/message-types.d.ts',
+        `
+// This is auto generated in the project.
+
+export declare const MessageTypes: ${JSON.stringify(MessageTypes)}
+export default MessageTypes
+`
+    );
