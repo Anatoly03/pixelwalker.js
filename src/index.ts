e4a944e81ac6248a1e9c9a41bc330b100fec8fbc
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-08-14 19:36:21 +0200

    export static variables to api user

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+//
+// Static Data & Variables retrieved from the server.
+//
+
+export { BlockMappings, BlockMappingsReverse } from './data/block-mappings.js';
+export { default as MessageTypes } from './data/message-types.js';
+export { default as PaletteFix } from './data/message-types.js';
+export { default as RoomTypes } from './data/room-types.js';
