ec64eeda95c9ab09e8141519f66fc3a6123fbcc3
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-11-23 15:10:19 +0100

    add asterisk event

--- src/connection.ts
+++ src/connection.ts
@@ -64,6 +64,14 @@ export default class GameConnection<Ready extends boolean = false> {
     }
 
     /**
+     * Adds the listener function to the end of the listeners array for all
+     * events. No checks are made to see if the listener has already been added.
+     * Multiple calls passing the same combination of `eventName="*"` and listener
+     * will result in the listener being added, and called, multiple times.
+     */
+    public on<Event extends (typeof MessageTypes)[number] & keyof ReceiveEvents>(eventName: "*", cb: (eventName: Event, ...args: ReceiveEvents[Event]) => void): this;
+
+    /**
      * Adds the listener function to the end of the listeners array for the
      * event named `eventName`. No checks are made to see if the listener has
      * already been added. Multiple calls passing the same combination of
@@ -87,6 +95,12 @@ export default class GameConnection<Ready extends boolean = false> {
         this.#receiver.once(event as any, callee);
         return this;
     }
+
+    /**
+     * Synchronously calls each of the listeners registered for all events (`eventName="*"`)
+     * in the order they were registered, passing the supplied arguments to each.
+     */
+    public emit<Index extends number, Event extends (typeof MessageTypes)[Index] & keyof ReceiveEvents>(listenerClass: "*", eventName: Event, ...args: ReceiveEvents[Event]): this;
 
     /**
      * Synchronously calls each of the listeners registered for the event named `eventName`,
@@ -152,6 +166,7 @@ export default class GameConnection<Ready extends boolean = false> {
                 case GameConnection.HeaderBytes.Message:
                     const messageId = buffer.read7BitInt();
                     const args = buffer.deserialize();
+                    this.emit('*', GameConnection.MessageTypes[messageId] as any, ...args);
                     this.emit(GameConnection.MessageTypes[messageId] as any, ...args);
                     break;
             }
@@ -183,13 +198,13 @@ export default class GameConnection<Ready extends boolean = false> {
 
         /**
          * @event PlayerInit
-         * 
+         *
          * Upon receiving the `PlayerInit` event, the server requires
          * the client to send the `PlayerInit` event as well back to
          * the server.
          */
-        this.once('PlayerInit', () => {
-            this.send('PlayerInit');
+        this.once("PlayerInit", () => {
+            this.send("PlayerInit");
         });
 
         return this as GameConnection<true>;
