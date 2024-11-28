cf403d98f20c64e129b6169c6602d341373310ab
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-11-28 21:33:34 +0100

    minor

--- localhost/index.d.ts
+++ localhost/index.d.ts
@@ -1,1 +1,14 @@
+/**
+ * If the `LOCALHOST` environment is set, then the API & Game server
+ * links will be local URLs corresponding with the ports of API and Game
+ * server.
+ * 
+ * @module pixelwalker.js/localhost
+ *
+ * @example
+ *
+ * ```ts
+ * import Client from 'pixelwalker.js/local'
+ * ```
+ */
 export * from "../dist/index.js";
--- localhost/index.js
+++ localhost/index.js
@@ -1,14 +1,2 @@
-/**
- * If the `LOCALHOST` environment is set, then the API & Game server
- * links will be local URLs corresponding with the ports of API and Game
- * server.
- *
- * @example
- *
- * ```ts
- * import Client from 'pixelwalker.js/local'
- * ```
- */
 process.env.LOCALHOST = "true";
-
 export * from "../dist/index.js";
