35411455983821fdec3480416e636fbc78b7df22
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-12-03 17:10:53 +0100

    refactor

--- src/world/structure.ts
+++ src/world/structure.ts
@@ -81,20 +81,27 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
      * Copy a subset of the structure defined by the coordinates.
      */
     public copy(x1: number, y1: number, x2: number, y2: number): Structure {
-        if (x2 < x1) { let tmp = x2; x2 = x1; x1 = tmp }
-        if (y2 < y1) { let tmp = y2; y2 = y1; y1 = tmp }
+        if (x2 < x1) {
+            let tmp = x2;
+            x2 = x1;
+            x1 = tmp;
+        }
+        if (y2 < y1) {
+            let tmp = y2;
+            y2 = y1;
+            y1 = tmp;
+        }
 
-        const world = new Structure(x2 - x1 + 1, y2 - y1 + 1)
+        const world = new Structure(x2 - x1 + 1, y2 - y1 + 1);
 
         for (let x = x1; x <= x2; x++)
             for (let y = y1; y <= y2; y++) {
-                if (x < 0 || y < 0 || x >= this.width || y >= this.height)
-                    continue
-                world.background[x - x1][y - y1] = this.background[x][y]
-                world.foreground[x - x1][y - y1] = this.foreground[x][y]
+                if (x < 0 || y < 0 || x >= this.width || y >= this.height) continue;
+                world.background[x - x1][y - y1] = this.background[x][y];
+                world.foreground[x - x1][y - y1] = this.foreground[x][y];
             }
 
-        return world
+        return world;
     }
 
     /**
@@ -116,9 +123,9 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
     /**
      * Read the structure from a parser implementation and
      * return a Structure instance. *The default parser is JSON.*
-     * 
+     *
      * @example
-     * 
+     *
      * ```ts
      * const value = fs.readFileSync("structure.json");
      * const data = Structure.fromString(value);
@@ -129,9 +136,9 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
     /**
      * Read the structure from a JSON parser implementation and
      * return a Structure instance.
-     * 
+     *
      * @example
-     * 
+     *
      * ```ts
      * const value = fs.readFileSync("structure.json");
      * const data = Structure.fromString(value, JSON);
@@ -142,12 +149,12 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
     /**
      * Read the structure from a custom parser implementation and
      * return a Structure instance.
-     * 
+     *
      * @example
-     * 
+     *
      * ```ts
      * import YAML from "yaml";
-     * 
+     *
      * const value = fs.readFileSync("structure.yaml");
      * const data = Structure.fromString(value, YAML);
      * ```
@@ -168,10 +175,10 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
         } else if (!data.palette) {
             throw new Error(`Missing palette data in structure.`);
         }
-        
+
         const structure = new Structure(data.width, data.height);
         structure.meta = data.meta ?? {};
-        
+
         for (let i = 0; i < Structure.LAYER_COUNT; i++) {
             const layer: string = data.layers[i];
             const blocks = layer.split(";").slice(0, data.width * data.height);
@@ -179,11 +186,11 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
 
             for (const bl of blocks) {
                 const [paletteId, args] = bl.split(".");
-                if (!paletteId) throw new Error('Malformed block data segment: ' + bl + ';');
+                if (!paletteId) throw new Error("Malformed block data segment: " + bl + ";");
 
                 const blockName = data.palette[parseInt(paletteId, 16)];
                 const block = new Block(blockName);
-                
+
                 if ((BlockArgs as any)[blockName]) {
                     const format = (BlockArgs as any)[blockName];
                     const buffer = BufferReader.from(Buffer.from(args, "hex"));
@@ -209,9 +216,9 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
     /**
      * Write the structure into a writeable stream and return an
      * JSON string representation of the structure.
-     * 
+     *
      * @example
-     * 
+     *
      * ```ts
      * const structure = ...;
      * const data = structure.toString(JSON);
@@ -223,12 +230,12 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
     /**
      * Write the structure into a writeable stream and return a custom
      * object encoding string.
-     * 
+     *
      * @example
-     * 
+     *
      * ```ts
      * import YAML from "yaml";
-     * 
+     *
      * const structure = ...;
      * const data = structure.toString(YAML);
      * fs.writeFileSync("structure.yaml", data);
@@ -250,7 +257,7 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
         };
 
         for (let i = 0; i < Structure.LAYER_COUNT; i++) {
-            let ENCODING: string = '';
+            let ENCODING: string = "";
 
             for (let y = 0; y < this.height; y++) {
                 for (let x = 0; x < this.width; x++) {
@@ -260,8 +267,8 @@ export default class Structure<Meta extends { [keys: number | string]: any } = {
                     let blockId = palette.indexOf(block.name);
 
                     ENCODING += blockId.toString(16).padStart(2, "0");
-                    if (block.data.length) ENCODING += '.' + block.serialize_args().toString('hex');
-                    ENCODING += ';';
+                    if (block.data.length) ENCODING += "." + block.serialize_args().toString("hex");
+                    ENCODING += ";";
                 }
             }
 
