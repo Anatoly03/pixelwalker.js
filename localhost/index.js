83cc75d8ebfc0f86158189ec04ea9a34795eb9a1
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-01-11 16:13:45 +0100

    unmake api

--- localhost/index.js
+++
@@ -1,8 +0,0 @@
-// Set the `LOCALHOST` flag to 'true', as in, deroute all client instances
-// the `127.0.0.1` instead of `pixelwalker.net`
-if (process)
-    process.env.LOCALHOST = "true";
-else
-    throw new Error('Cannot import `pixelwalker.js/localhost` in an environment, where `process` is not defined.');
-
-export * from "../dist/index.js";
--- src/types/join-data.ts
+++
@@ -1,8 +0,0 @@
-export type JoinData = Partial<{
-    world_title: string;
-    world_width: 636 | 400 | 375 | 350 | 325 | 300 | 275 | 250 | 225 | 200 | 175 | 150 | 125 | 100 | 75 | 50;
-    world_height: 400 | 375 | 350 | 325 | 300 | 275 | 250 | 225 | 200 | 175 | 150 | 125 | 100 | 75 | 50;
-    spawnId: number;
-}>;
-
-export default JoinData;
