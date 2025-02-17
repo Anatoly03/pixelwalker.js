32783658ead1c0a2f1680392f280be1782f74341
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-02-17 16:21:22 +0100

    add more index exports

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,14 @@ export { default as StructureParser } from "./world/parser/index.js";
 export { default as Structure } from "./world/structure.js";
 export { default as World } from "./world/world.js";
 
+export type { GamePlayer } from "./types/game-player.js";
+export type { JoinData } from "./types/join-data.js";
+export type { LayerPosition } from "./types/layer-position.js";
+// TODO export private world
+// TODO export public profile
+// TODO export public world
+
+export type { WorldMeta } from "./types/world-meta.js";
+export type { WorldPosition } from "./types/world-position.js";
+
 export * as Protocol from './protocol/world_pb.js';
