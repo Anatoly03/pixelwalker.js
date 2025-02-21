1f068082767864e0d6836088d18410bc9a7480b8
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-02-21 21:14:11 +0100

    add spiral paste

--- src/world/paste-animation.ts
+++ src/world/paste-animation.ts
@@ -35,11 +35,11 @@ export function* TOPLEFT_TO_BOTTOMRIGHT_HORIZONTAL(structure: Structure) {
 }
 
 /**
- * Paste the structure from left to right, top to bottom.
+ * Paste the structure from top to bottom, left to right.
  */
 export function* TOPLEFT_TO_BOTTOMRIGHT_VERTICAL(structure: Structure) {
-    for (let y = 0; y < structure.height; y++) {
-        for (let x = 0; x < structure.width; x++) {
+    for (let x = 0; x < structure.width; x++) {
+        for (let y = 0; y < structure.height; y++) {
             const positions: WorldPosition[] = [];
 
             for (const [layer, _] of structure.layers()) {
@@ -48,5 +48,76 @@ export function* TOPLEFT_TO_BOTTOMRIGHT_VERTICAL(structure: Structure) {
 
             yield positions;
         }
+    }
+}
+
+/**
+ * Places the blocks in a clockwise rectangular spiral.
+ */
+export function* SPIRAL_CLOCKWISE(structure: Structure) {
+    let topRead = 0;
+    let rightRead = structure.width - 1;
+    let bottomRead = structure.height - 1;
+    let leftRead = 0;
+
+    while (true) {
+        let modified = false;
+
+        // Left -> Right
+        for (let x = leftRead; x <= rightRead; x++) {
+            const positions: WorldPosition[] = [];
+
+            for (const [layer, _] of structure.layers()) {
+                positions.push({ x, y: topRead, layer });
+            }
+
+            modified = true;
+            yield positions;
+        }
+
+        // Top -> Bottom
+        for (let y = topRead + 1; y <= bottomRead; y++) {
+            const positions: WorldPosition[] = [];
+
+            for (const [layer, _] of structure.layers()) {
+                positions.push({ x: rightRead, y, layer });
+            }
+
+            modified = true;
+            yield positions;
+        }
+
+        // Right -> Left
+        for (let x = rightRead - 1; x >= leftRead; x--) {
+            const positions: WorldPosition[] = [];
+
+            for (const [layer, _] of structure.layers()) {
+                positions.push({ x, y: bottomRead, layer });
+            }
+
+            modified = true;
+            yield positions;
+        }
+
+        // Bottom -> Top
+        for (let y = bottomRead - 1; y > topRead; y--) {
+            const positions: WorldPosition[] = [];
+
+            for (const [layer, _] of structure.layers()) {
+                positions.push({ x: leftRead, y, layer });
+            }
+
+            modified = true;
+            yield positions;
+
+            if (y === topRead + 1) break;
+        }
+
+        topRead++;
+        rightRead--;
+        bottomRead--;
+        leftRead++;
+
+        if (!modified) break;
     }
 }
